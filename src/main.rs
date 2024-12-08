6d5cbf03ddff61e4110147b8f1ae60ba49114910
Tushar Mathur <tusharmath@gmail.com>
2024-12-08 12:31:00 -0800

    add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{self, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/greet", get(|| async { "Hello World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
