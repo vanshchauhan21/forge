10eaedd73b4f409aa33791dfe33722d325f30086
Tushar Mathur <tusharmath@gmail.com>
2024-12-17 09:39:44 -0800

    remove unused dependencies

--- crates/forge-ci/tests/ci.rs
+++
@@ -1,6 +0,0 @@
-use gh_workflow_tailcall::*;
-
-#[test]
-fn generate() {
-    Workflow::default().auto_fix(true).generate().unwrap();
-}
---
+++ crates/forge-cli/src/log.rs
@@ -0,0 +1,24 @@
+use clap::ValueEnum;
+use tracing::level_filters::LevelFilter;
+
+#[derive(Default, Debug, Clone, ValueEnum)]
+pub enum LogLevel {
+    Trace,
+    Debug,
+    #[default]
+    Info,
+    Warn,
+    Error,
+}
+
+impl From<LogLevel> for LevelFilter {
+    fn from(level: LogLevel) -> Self {
+        match level {
+            LogLevel::Trace => LevelFilter::TRACE,
+            LogLevel::Debug => LevelFilter::DEBUG,
+            LogLevel::Info => LevelFilter::INFO,
+            LogLevel::Warn => LevelFilter::WARN,
+            LogLevel::Error => LevelFilter::ERROR,
+        }
+    }
+}
