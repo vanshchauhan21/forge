ad82c16893dd07aff0ee4874c43472a95bbcc5a4
Tushar Mathur <tusharmath@gmail.com>
2024-12-19 21:57:27 -0800

    add new provider models

--- crates/forge-provider/src/model.rs
+++ crates/forge-provider/src/model.rs
@@ -1,146 +1,67 @@
-use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
-pub struct Model {
-    pub id: String,
-    pub name: String,
-    pub created: u64,
-    pub description: String,
-    pub context_length: u64,
-    pub architecture: Architecture,
-    pub pricing: Pricing,
-    pub top_provider: TopProvider,
-    pub per_request_limits: Option<serde_json::Value>,
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
-pub struct Architecture {
-    pub modality: String,
-    pub tokenizer: String,
-    pub instruct_type: Option<String>,
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
-pub struct Pricing {
-    pub prompt: String,
-    pub completion: String,
-    pub image: String,
-    pub request: String,
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
-pub struct TopProvider {
-    pub context_length: Option<u64>,
-    pub max_completion_tokens: Option<u64>,
-    pub is_moderated: bool,
-}
-
-#[derive(Debug, Deserialize, Clone, PartialEq, Serialize)]
-pub struct ListModelResponse {
-    pub data: Vec<Model>,
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone, Default)]
 pub struct Request {
-    pub messages: Option<Vec<Message>>,
-    pub prompt: Option<String>,
-    pub model: Option<String>,
-    pub response_format: Option<ResponseFormat>,
-    pub stop: Option<Vec<String>>,
-    pub stream: Option<bool>,
-    pub max_tokens: Option<u32>,
-    pub temperature: Option<f32>,
-    pub tools: Option<Vec<Tool>>,
-    pub tool_choice: Option<ToolChoice>,
-    pub seed: Option<u32>,
-    pub top_p: Option<f32>,
-    pub top_k: Option<u32>,
-    pub frequency_penalty: Option<f32>,
-    pub presence_penalty: Option<f32>,
-    pub repetition_penalty: Option<f32>,
-    pub logit_bias: Option<std::collections::HashMap<u32, f32>>,
-    pub top_logprobs: Option<u32>,
-    pub min_p: Option<f32>,
-    pub top_a: Option<f32>,
-    pub prediction: Option<Prediction>,
-    pub transforms: Option<Vec<String>>,
-    pub models: Option<Vec<String>>,
-    pub route: Option<String>,
-    pub provider: Option<ProviderPreferences>,
+    context: Vec<AnyMessage>,
+    available_tools: Vec<Tool>,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct TextContent {
-    // TODO: could be an enum
-    pub r#type: String,
-    pub text: String,
+pub struct System {}
+pub struct User {}
+pub struct Assistant {}
+
+pub trait Role {
+    fn name() -> String;
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct ImageContentPart {
-    pub r#type: String,
-    pub image_url: ImageUrl,
+impl Role for System {
+    fn name() -> String {
+        "system".to_string()
+    }
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct ImageUrl {
-    pub url: String,
-    pub detail: Option<String>,
+impl Role for User {
+    fn name() -> String {
+        "user".to_string()
+    }
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub enum ContentPart {
-    Text(TextContent),
-    Image(ImageContentPart),
+impl Role for Assistant {
+    fn name() -> String {
+        "assistant".to_string()
+    }
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Message {
-    pub role: String,
-    pub content: ContentPart,
-    pub name: Option<String>,
+pub struct Message<Role> {
+    content: String,
+    role: Role,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct FunctionDescription {
-    pub description: Option<String>,
-    pub name: String,
-    pub parameters: serde_json::Value,
+pub enum AnyMessage {
+    System(Message<System>),
+    User(Message<User>),
+    Assistant(Message<Assistant>),
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct ToolId(String);
+
 pub struct Tool {
-    // TODO: should be an enum
-    pub r#type: String,
-    pub function: FunctionDescription,
+    id: ToolId,
+    description: String,
+    input_schema: JsonSchema,
+    output_schema: JsonSchema,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub enum ToolChoice {
-    None,
-    Auto,
-    Function { name: String },
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct ResponseFormat {
-    pub r#type: String,
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Prediction {
-    pub r#type: String,
-    pub content: String,
-}
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Response {
-    pub status: String,
-    pub data: Option<serde_json::Value>,
-    pub error: Option<String>,
+    message: Message<Assistant>,
+    call_tool: Vec<CallTool>,
 }
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct ProviderPreferences {
-    // Define fields as necessary
+pub struct CallId(String);
+
+pub struct CallTool {
+    id: CallId,
+    tool_id: ToolId,
+    input: Value,
 }
+
+pub struct JsonSchema(Value);
