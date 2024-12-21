6e944ae6d7b2e27be6363f252a039cb0a3f6a5bd
Tushar Mathur <tusharmath@gmail.com>
2024-12-21 10:35:04 -0800

    refactor: remove unused forge-engine crate and clean up main.rs

--- crates/forge-engine/src/error.rs
+++
@@ -1,9 +0,0 @@
-use derive_more::derive::From;
-
-#[derive(Debug, From)]
-pub enum Error {
-    Serde(serde_json::Error),
-    Provider(forge_provider::error::Error),
-}
-
-pub(crate) type Result<T> = std::result::Result<T, Error>;
--- crates/forge-engine/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod error;
-#[allow(unused)]
-mod forge;
-#[allow(unused)]
-pub mod model;
-pub use forge::*;
