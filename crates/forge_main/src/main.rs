35731728ff2a1594a4a745ab9285d5442428c58e
Tushar Mathur <tusharmath@gmail.com>
2025-01-10 13:58:36 -0800

    refactor: rename path to exec in CLI and streamline content initialization

--- crates/forge_main/src/main.rs
+++ crates/forge_main/src/main.rs
@@ -9,79 +9,85 @@ use tokio_stream::StreamExt;
 
 #[derive(Parser)]
 struct Cli {
-    path: Option<String>,
+    exec: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    if let Some(path) = cli.path {
-        let api = API::init().await?;
-
+    let mut content = if let Some(path) = cli.exec {
         let cwd = std::env::current_dir()?;
         let full_path = cwd.join(path);
-        let mut content = tokio::fs::read_to_string(full_path).await?;
+        tokio::fs::read_to_string(full_path)
+            .await?
+            .trim()
+            .to_string()
+    } else {
+        inquire::Text::new("")
+            .with_help_message("How can I help?")
+            .prompt()
+            .unwrap()
+            .to_string()
+    };
 
-        println!("{}", content.trim());
-        let mut current_conversation_id = None;
-        loop {
-            let model = ModelId::from_env(api.env());
-            let chat = ChatRequest {
-                content: content.clone(),
-                model,
-                conversation_id: current_conversation_id,
-            };
+    println!("{}", content.trim());
+    let mut current_conversation_id = None;
+    let api = API::init().await?;
+    loop {
+        let model = ModelId::from_env(api.env());
+        let chat = ChatRequest {
+            content: content.clone(),
+            model,
+            conversation_id: current_conversation_id,
+        };
 
-            let mut stream = api.chat(chat).await?;
-            while let Some(message) = stream.next().await {
-                match message.unwrap() {
-                    ChatResponse::Text(text) => {
-                        print!("{}", text);
-                    }
-                    ChatResponse::ToolCallDetected(_) => {}
-                    ChatResponse::ToolCallArgPart(arg) => {
-                        print!("{}", arg);
-                    }
-                    ChatResponse::ToolCallStart(tool_call_full) => {
-                        println!(
-                            "\n{} {}",
-                            "Tool use detected:".green(),
-                            tool_call_full.name.as_str()
-                        );
-                    }
-                    ChatResponse::ToolCallEnd(tool_result) => {
-                        println!("{}", tool_result.content);
-                    }
-                    ChatResponse::ConversationStarted(conversation_id) => {
-                        current_conversation_id = Some(conversation_id);
-                    }
-                    ChatResponse::ModifyContext(_) => {}
-                    ChatResponse::Complete => {}
-                    ChatResponse::Error(err) => {
-                        panic!("{:?}", err);
-                    }
-                    ChatResponse::PartialTitle(_) => {}
-                    ChatResponse::CompleteTitle(title) => {
-                        println!("title: {}", title);
-                    }
+        let mut stream = api.chat(chat).await?;
+        while let Some(message) = stream.next().await {
+            match message.unwrap() {
+                ChatResponse::Text(text) => {
+                    print!("{}", text);
                 }
-
-                std::io::stdout().flush().unwrap();
+                ChatResponse::ToolCallDetected(_) => {}
+                ChatResponse::ToolCallArgPart(arg) => {
+                    print!("{}", arg);
+                }
+                ChatResponse::ToolCallStart(tool_call_full) => {
+                    println!(
+                        "\n{} {}",
+                        "Tool use detected:".green(),
+                        tool_call_full.name.as_str()
+                    );
+                }
+                ChatResponse::ToolCallEnd(tool_result) => {
+                    println!("{}", tool_result);
+                }
+                ChatResponse::ConversationStarted(conversation_id) => {
+                    current_conversation_id = Some(conversation_id);
+                }
+                ChatResponse::ModifyContext(_) => {}
+                ChatResponse::Complete => {}
+                ChatResponse::Error(err) => {
+                    panic!("{:?}", err);
+                }
+                ChatResponse::PartialTitle(_) => {}
+                ChatResponse::CompleteTitle(title) => {
+                    println!("title: {}", title);
+                }
             }
 
-            println!();
-            content = inquire::Text::new("")
-                .with_help_message("type '/done' to end this conversation.")
-                .prompt()
-                .unwrap();
-            if content.trim() == "/done" {
-                break;
-            }
+            std::io::stdout().flush().unwrap();
         }
 
-        Ok(())
-    } else {
-        Ok(API::init().await?.launch().await?)
+        println!();
+        content = inquire::Text::new("")
+            .with_help_message("type '/done' to end this conversation.")
+            .prompt()
+            .unwrap();
+        if content.trim() == "/done" {
+            break;
+        }
     }
+
+    Ok(())
 }
