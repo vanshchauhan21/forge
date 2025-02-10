94d29fb046c10b3a7649168f3e7730f4d1a90a05
Amit Singh <amitksingh1490@gmail.com>
2025-02-09 18:33:10 -0800

    feat: add support for more target architectures and windows os (#331)
    Co-authored-by: amitksingh1490 <amitksingh1490@users.noreply.github.com>

--- crates/forge_ci/tests/ci.rs
+++ crates/forge_ci/tests/ci.rs
@@ -26,6 +26,12 @@ fn generate() {
                 "binary_path": "target/x86_64-unknown-linux-gnu/release/forge"
             },
             {
+                "os": "ubuntu-latest",
+                "target": "aarch64-unknown-linux-gnu",
+                "binary_name": "forge-aarch64-unknown-linux-gnu",
+                "binary_path": "target/aarch64-unknown-linux-gnu/release/forge"
+            },
+            {
                 "os": "macos-latest",
                 "target": "x86_64-apple-darwin",
                 "binary_name": "forge-x86_64-apple-darwin",
@@ -36,6 +42,18 @@ fn generate() {
                 "target": "aarch64-apple-darwin",
                 "binary_name": "forge-aarch64-apple-darwin",
                 "binary_path": "target/aarch64-apple-darwin/release/forge"
+            },
+            {
+                "os": "windows-latest",
+                "target": "x86_64-pc-windows-msvc",
+                "binary_name": "forge-x86_64-pc-windows-msvc.exe",
+                "binary_path": "target/x86_64-pc-windows-msvc/release/forge.exe"
+            },
+            {
+                "os": "windows-latest",
+                "target": "aarch64-pc-windows-msvc",
+                "binary_name": "forge-aarch64-pc-windows-msvc.exe",
+                "binary_path": "target/aarch64-pc-windows-msvc/release/forge.exe"
             }
         ]
     });
